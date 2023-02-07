676f9340afb996a36380b1ae7a3b128b53f1c6ab
Evening <evening@Admins-iMac.local>
2023-02-07 17:34:30 +0800

    cat done

--- server.js
+++ server.js
@@ -126,6 +126,48 @@ server.get("/categories", (req, res) => {
   res.json();
 });
 
+server.get("/categories/:id", (req, res) => {
+  try {
+    const { id } = req.params;
+    const categoriesData = fs.readFileSync("categories.json", "utf-8");
+    const data = JSON.parse(categoriesData);
+    const category = data.categories.find((el) => el.id === id);
+    res.status(201).json({ category });
+  } catch (err) {
+    return res.status(400).json({ message: err.message });
+  }
+});
+
+server.put("/categories/:id", (req, res) => {
+  try {
+    const { id } = req.params;
+    const { title } = req.body;
+    const categoriesData = fs.readFileSync("categories.json", "utf-8");
+    const data = JSON.parse(categoriesData);
+    const findIndex = data.categories.findIndex((el) => el.id === id);
+    data.categories[findIndex].title = title;
+    fs.writeFileSync("categories.json", JSON.stringify(data));
+    res
+      .status(201)
+      .json({ message: `${id}: Category title updated to ${title}` });
+  } catch (err) {
+    return res.status(400).json({ message: err.message });
+  }
+});
+
+server.delete("/categories/:id", (req, res) => {
+  try {
+    const { id } = req.params;
+    const categoriesData = fs.readFileSync("categories.json", "utf-8");
+    const data = JSON.parse(categoriesData);
+    const findIndex = data.categories.findIndex((el) => el.id === id);
+    data.categories.splice(findIndex, 1);
+    fs.writeFileSync("categories.json", JSON.stringify(data));
+    res.status(201).json({ message: `${id}: Category deleted` });
+  } catch (err) {
+    return res.status(400).json({ message: err.message });
+  }
+});
 // End of Category
 server.listen(port, () => {
   console.log(`Server aslaa port ${port}`);
